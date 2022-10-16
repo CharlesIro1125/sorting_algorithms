e33201b6c963a2a7063f937934d2adab6defd8df
Charles Obinna Iro <irocharles22@gmail.com>
2022-10-15 22:56:31 -0700

    sorting algorithm

--- sort.h
+++ sort.h
@@ -23,4 +23,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 listint_t *swap(listint_t *head, listint_t **list);
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
 #endif
