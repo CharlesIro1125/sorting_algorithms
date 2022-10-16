8802a4c01db2989cc3045ac9da273db22601ac73
Charles Obinna Iro <irocharles22@gmail.com>
2022-10-15 23:15:30 -0700

    sorting algorithm

--- sort.h
+++ sort.h
@@ -15,6 +15,20 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
+/* Comparison direction macros for bitonic sort */
+#define UP 0
+#define DOWN 1
+
+/**
+ * enum bool - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
